51a9b7a4f9f46e2a41cb3b8eff609c63c4e9a53c
G4M3R L1F3 <patrick-doiron@hotmail.com>
2025-01-22 01:23:05 -0400

    Added new items w/ pictures and wrote region rules

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -61,7 +61,7 @@ function has_paper() -- was "paper"
 end
 
 function can_plant_beans() -- was "plant_beans"
-    return can_get_magic_beans() and (has("bottle") or has("storms"))
+    return can_get_magic_beans() and (has("bottle") or can_play_storms())
 end
 
 function can_use_powder_keg() -- was "use_keg"
@@ -89,18 +89,52 @@ function can_bring_to_player()
 end
 
 
--- Difficulty specific settings (WIP)
+-- Easy difficulty rules
+function baby_has_bombchus()
+    return has("bombchu1") and has("bombchu5") and has("bombchu10")
+end
 
--- Baby and normal rules have slight differences!!!
---function can_reach_scarecrow()
---    return can_reach_astral_observatory() --[[or can_reach_trading_post()]]and has("hookshot")
---end
+function baby_has_explosives()
+    return has("bombs") and baby_has_bombchus() and has("blast")
+end
 
--- Baby and normal rules have slight differences!!!
+function baby_has_hard_projectiles()
+    return has("bow") and has("zora") and has("hookshot")
+end
+
+function baby_has_projectiles()
+    return has("deku") and has("magic") and baby_has_hard_projectiles()
+end
+
+function baby_can_smack_hard()
+    return has("sword") and has("fiercedeity") and has("fairysword") and has("goron") and has("zora")
+end
+
+function baby_can_smack()
+    return has("deku") and baby_can_smack_hard()
+end
+
+function baby_has_paper()
+    return has("landdeed") and has("swampdeed") and has("mountaindeed") or has("oceandeed") or has ("kafeiletter") or has ("express")
+end
+
+function baby_has_bottle()
+    return has("redpotion") and has("chateau")
+end
+
+function baby_can_plant_beans()
+    return can_get_magic_beans() and baby_has_bottle() and can_play_storms()
+end
+
+function baby_can_bring_to_player()
+    return has("hookshot") and has("zora")
+end
+
 --function can_reach_seahorse()
 --    return can_reach_fisherman_house()
 --end
 
+--[[Must find a way to read shop prices
 function can_purchase()
     if price > 200 then
         return has("giantswallet")
@@ -108,7 +142,7 @@ function can_purchase()
         return has("adultswallet")
     else
     end
-end
+end]]
 
 function anju_kafei() -- was "anju_kafei"
     return has("kafeimask") and play_eponas() and has("kafeiletter") and has("pendant") and has("hookshot") and 
@@ -122,32 +156,185 @@ function moon() -- was "moon"
     return has("ocarina") and has("oath") and has("odolwa") and has("goht") and has("gyorg") and has("twinmold")
 end
 
-function south_swamp() -- was "south_swamp"
-    return projectiles_hard() and has("deku") and has("redpotion")
+--- SWAMP AREA ---
+-- Southern Swamp -> Southern Swamp (Deku Palace)
+function baby_south_swamp()
+    return has("redpotion") and baby_has_hard_projectiles() and has("deku")
+end
+function south_swamp()
+    return has("redpotion") or (projectiles_hard() and has("deku")) or (has("pictobox") and has("deku"))
 end
 
-function deku_palace() -- was "deku_palace"
-    return south_swamp() and has("deku")
+-- Southern Swamp (Deku Palace) -> Swamp Spider House
+function baby_swamp_skull_house()
+    return baby_south_swamp() and has("deku") and can_use_fire_arrows()
 end
-
 function swamp_skull_house() -- was "swamp_skull_house"
-    return south_swamp() and has("deku") and use_fire_arrows()
-end
-
-function woodfall() -- was "woodfall"
     return south_swamp() and has("deku")
 end
 
-function enter_woodfall() -- was "enter_woodfall"
-    return has("deku") and has("sonata") and has("ocarina")
+-- Southern Swamp (Deku Palace) -> Deku Palace
+function baby_deku_palace()
+    return baby_south_swamp() and has("deku")
+end
+function deku_palace()
+    return south_swamp() and has("deku")
 end
 
+-- Southern Swamp (Deku Palace) -> Woodfall
+function baby_woodfall()
+    return baby_south_swamp() and has("deku")
+end
+function woodfall()
+    return south_swamp() and has("deku")
+end
+
+-- Woodfall -> Woodfall Temple
+function baby_woodfall_temple()
+    return baby_woodfall() and can_play_sonata()
+end
 function woodfall_temple() -- was "woodfall_temple"
-    return woodfall() and play_sonata()
+    return woodfall() and can_play_sonata()
 end
 
+-- What's needed to defeat Odolwa
+function baby_clear_woodfall()
+    return baby_woodfall_temple() and has("bow") and baby_can_smack_hard() and has("bk_wf") and has("odolwa")
+end
 function clear_woodfall() -- was "clear_woodfall"
-    return (has("bk_wf") or has("odolwa")) and enter_woodfall() and has("bow")
+    return woodfall_temple() and has("bow") and can_smack() and (has("bk_wf") or has("odolwa"))
+end
+
+
+--- MOUNTAIN AREA ---
+-- Termina Field -> Path to Mountain Village
+-- Easy and normal
+function path_mountain()
+    return has("bow")
+end
+
+-- Path to Mountain Village -> Mountain Village
+function baby_moutain_village()
+    return path_mountain() and has("goron") and baby_has_explosives() and can_use_fire_arrows()
+end
+function mountain_village()
+    return path_mountain() and (has("goron") or has_explosives() or can_use_fire_arrows())
+end
+
+-- Path to Snowhead -> Snowhead Temple
+function baby_snowhead_temple()
+    return baby_moutain_village() and has("goron") and can_play_lullaby() and has("magic")
+end
+function snowhead_temple()
+    return mountain_village() and has("goron") and can_play_lullaby() and has("magic")
+end
+
+-- What's needed to defeat Goht ***will likely need to revisit in the future
+function baby_clear_snowhead()
+    return baby_snowhead_temple() and can_use_fire_arrows() and has("bosskey_sh") and has("smallkey_sh")--[[need to revisit]] and baby_has_explosives()
+end
+function clear_snowhead()
+    return snowhead_temple() and--[[(can_use_fire_arrows() and has("bosskey_sh") and has("smallkey_sh", 3) and has_explosives()) or]] (can_use_fire_arrows() and has("smallkey") and has("bosskey_sh")) or (can_use_fire_arrows() and has("goht"))
+end
+
+
+--- GREAT BAY AREA ---
+-- Termina Field -> Great Bay
+-- Easy and normal
+function great_bay()
+    return can_play_eponas()
+end
+
+-- Great Bay -> Ocean Spider House
+function baby_ocean_spider_house()
+    return great_bay() and baby_has_explosives() and has("hookshot")
+end
+function ocean_spider_house()
+    return great_bay() and has_explosives()
+end
+
+-- Great Bay -> Pirates' Fortress
+-- Easy and normal
+function pirates_fortress()
+    return great_bay() and has("zora")
+end
+
+-- Pirates' Fortress -> Pirates' Fortress (Interior)
+function baby_pirates_fortress_interior()
+    return pirates_fortress() and has("goron") and has("hookshot")
+end
+function pirates_fortress_interior()
+    return pirates_fortress() and (has("goron") or has("hookshot"))
+end
+
+-- Zora Cape -> Great Bay Temple
+-- Easy and normal
+function great_bay_temple()
+    return great_bay() and can_play_bossa_nova() and has("hookshot") and has("zora")
+end
+
+-- What's needed to defeat Gyorg
+function baby_clear_greatbay()
+    return has("zora") and can_use_ice_arrows() and has("fairymask") and can_use_fire_arrows() and has("smallkey_gb") and has("bosskey_gb") and has("bow")
+end
+function clear_greatbay()
+    return has("zora") and can_use_ice_arrows() and can_use_fire_arrows() and (has("bosskey_gb") or has("gyorg"))
+end
+
+
+--- IKANA REGION ---
+-- Road to Ikana -> Ikana Graveyard
+-- Easy and normal
+function graveyard()
+    return can_play_eponas()
+end
+
+--Road to Ikana -> Ikana Canyon
+function baby_ikana_canyon()
+    return can_play_eponas() and has("hookshot") and has("garo") and has("gibdo")
+end
+function ikana_canyon()
+    return can_play_eponas() and has("hookshot") and (has("garo") or has("gibdo"))
+end
+
+-- Ikana Canyon -> Secret Shrine
+function baby_secret_shrine()
+    return baby_ikana_canyon() and can_use_light_arrows()
+end
+function secret_shrine()
+    return ikana_canyon()
+end
+
+-- Ikana Canyon -> Beneath the Well
+function baby_well()
+    return baby_ikana_canyon() and can_use_ice_arrows() and has("gibdo") and baby_has_bottle()
+end
+function well()
+    return ikana_canyon() and can_use_ice_arrows() and has("gibdo") and has("bottle")
+end
+
+-- Ikana Canyon -> Ikana Castle
+function baby_ikana_castle()
+    return baby_ikana_canyon() and can_use_ice_arrows() and can_use_light_arrows() and has("gibdo") and has("mirrorshield") and baby_has_bottle()
+end
+function ikana_castle()
+    return ikana_canyon() and can_use_ice_arrows() and (can_use_light_arrows() or has("mirrorshield"))
+end
+
+-- Stone Tower -> Stone Tower Temple
+function baby_stone_tower_temple()
+    return baby_ikana_canyon() and can_use_ice_arrows() and can_play_elegy() and has("goron") and has("zora")
+end
+function stone_tower_temple()
+    return ikana_canyon() and can_use_ice_arrows() and can_play_elegy() and has("goron") and has("zora")
+end
+
+-- Stone Tower -> Stone Tower (Inverted)
+function baby_inverted_stone_tower()
+    return baby_stone_tower_temple() and can_use_light_arrows()
+end
+function inverted_stone_tower()
+    return stone_tower_temple() and can_use_light_arrows()
 end
 
 -----

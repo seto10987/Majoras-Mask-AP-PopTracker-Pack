f7c6ac92ed339e0f336bd77ae254ea08ccaaa8e3
G4M3R L1F3 <patrick-doiron@hotmail.com>
2025-01-22 04:08:57 -0400

    Tweaked logic alongside Pixel

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -130,9 +130,13 @@ function baby_can_bring_to_player()
     return has("hookshot") and has("zora")
 end
 
---function can_reach_seahorse()
---    return can_reach_fisherman_house()
---end
+function baby_can_reach_seahorse()
+    return great_bay() and has("zora") and has("hookshot") and has("goron") and has("pictobox")
+end
+
+function easy_can_reach_seahorse()
+    return great_bay() and has("zora") and (has("hookshot") or has("goron")) and has("pictobox")
+end
 
 --[[Must find a way to read shop prices
 function can_purchase()
@@ -191,7 +195,7 @@ end
 
 -- Woodfall -> Woodfall Temple
 function baby_woodfall_temple()
-    return baby_woodfall() and can_play_sonata()
+    return baby_woodfall() and can_play_sonata() and has("bosskey_wt") and has("smallkey_wt")
 end
 function woodfall_temple() -- was "woodfall_temple"
     return woodfall() and can_play_sonata()
@@ -199,10 +203,10 @@ end
 
 -- What's needed to defeat Odolwa
 function baby_clear_woodfall()
-    return baby_woodfall_temple() and has("bow") and baby_can_smack_hard() and has("bk_wf") and has("odolwa")
+    return baby_woodfall_temple() and has("bow") and baby_can_smack_hard() and has("bosskey_wf") and has("odolwa")
 end
 function clear_woodfall() -- was "clear_woodfall"
-    return woodfall_temple() and has("bow") and can_smack() and (has("bk_wf") or has("odolwa"))
+    return woodfall_temple() and has("bow") and can_smack() and (has("bosskey_wf") or has("odolwa"))
 end
 
 
@@ -223,7 +227,7 @@ end
 
 -- Path to Snowhead -> Snowhead Temple
 function baby_snowhead_temple()
-    return baby_moutain_village() and has("goron") and can_play_lullaby() and has("magic")
+    return baby_moutain_village() and has("goron") and can_play_lullaby() and has("magic") and has("bosskey_sh") and has("smallkey_sh", 3)
 end
 function snowhead_temple()
     return mountain_village() and has("goron") and can_play_lullaby() and has("magic")
@@ -231,10 +235,10 @@ end
 
 -- What's needed to defeat Goht ***will likely need to revisit in the future
 function baby_clear_snowhead()
-    return baby_snowhead_temple() and can_use_fire_arrows() and has("bosskey_sh") and has("smallkey_sh")--[[need to revisit]] and baby_has_explosives()
+    return baby_snowhead_temple() and can_use_fire_arrows() and baby_has_explosives()
 end
 function clear_snowhead()
-    return snowhead_temple() and--[[(can_use_fire_arrows() and has("bosskey_sh") and has("smallkey_sh", 3) and has_explosives()) or]] (can_use_fire_arrows() and has("smallkey") and has("bosskey_sh")) or (can_use_fire_arrows() and has("goht"))
+    return snowhead_temple() and (can_use_fire_arrows() and has("smallkey", 1) and has("bosskey_sh")) or (can_use_fire_arrows() and has("goht"))
 end
 
 
@@ -268,17 +272,19 @@ function pirates_fortress_interior()
 end
 
 -- Zora Cape -> Great Bay Temple
--- Easy and normal
+function baby_great_bay_temple()
+    return great_bay() and can_play_bossa_nova() and has("hookshot") and has("zora") and has("bosskey_gb") and has("smallkey_gb")
+end
 function great_bay_temple()
     return great_bay() and can_play_bossa_nova() and has("hookshot") and has("zora")
 end
 
 -- What's needed to defeat Gyorg
 function baby_clear_greatbay()
-    return has("zora") and can_use_ice_arrows() and has("fairymask") and can_use_fire_arrows() and has("smallkey_gb") and has("bosskey_gb") and has("bow")
+    return baby_great_bay_temple() has("zora") and can_use_ice_arrows() and can_use_fire_arrows() and has("bow")
 end
 function clear_greatbay()
-    return has("zora") and can_use_ice_arrows() and can_use_fire_arrows() and (has("bosskey_gb") or has("gyorg"))
+    return great_bay_temple() has("zora") and can_use_ice_arrows() and can_use_fire_arrows() and (has("bosskey_gb") or has("gyorg"))
 end
 
 
@@ -323,7 +329,7 @@ end
 
 -- Stone Tower -> Stone Tower Temple
 function baby_stone_tower_temple()
-    return baby_ikana_canyon() and can_use_ice_arrows() and can_play_elegy() and has("goron") and has("zora")
+    return baby_ikana_canyon() and can_use_ice_arrows() and can_play_elegy() and has("goron") and has("zora") and has("bosskey_st") and has("smallkey_st", 4)
 end
 function stone_tower_temple()
     return ikana_canyon() and can_use_ice_arrows() and can_play_elegy() and has("goron") and has("zora")
